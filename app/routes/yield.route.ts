0320489e9aa55448f33e48a84cf57cfa5879036e
vpineda1996 <vpineda1996@gmail.com>
2018-11-20 12:29:51 -0800

    remove useless files

--- app/routes/yield.route.ts
+++ app/routes/yield.route.ts
@@ -3,9 +3,12 @@ import * as controller from '../controllers/yield.controller';
 
 export default (api: restify.Server) => {
 
+  /** GET /api/yield/:studyId - Get rows of the given studyId */
   api.get('/api/yield/:studyId', controller.load, controller.get);
 
+  /** POST /api/yield - Create new yield entry for a given study */
   api.post('/api/yield', controller.create);
 
+  /** DELETE /api/yield/:studyId - Delete all of the rows pertaining to studyId */
   api.del('/api/yield/:studyId', controller.load, controller.remove);
 };
