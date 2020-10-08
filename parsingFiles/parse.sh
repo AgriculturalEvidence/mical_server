db53b2cc4074fa126e67edd643e1d77b533e76ba
john <likisongproduct@gmail.com>
2020-10-08 01:30:16 -0700

    working parser to mongo cloud atlas and local env with client

--- parsingFiles/parse.sh
+++ parsingFiles/parse.sh
@@ -1,3 +1,3 @@
-mongoimport --uri mongodb+srv://admin:admin123@cluster0.5he4u.mongodb.net/Cluster0 --collection interventions --type csv --headerline --file parsingFiles/intervention.csv
+mongoimport --uri mongodb+srv://admin:admin123@cluster0.5he4u.mongodb.net/Cluster0 --collection interventions --drop --type csv --headerline --file parsingFiles/intervention.csv
 
-mongoimport --uri mongodb+srv://admin:admin123@cluster0.5he4u.mongodb.net/Cluster0 --collection yields --type json --jsonArray --file parsingFiles/yields.json  
+mongoimport --uri mongodb+srv://admin:admin123@cluster0.5he4u.mongodb.net/Cluster0 --collection yields --drop --type json --jsonArray --file parsingFiles/yields.json
--- parsingFiles/parser.ts
+++ parsingFiles/parser.ts
@@ -1,21 +1,35 @@
 const CSVToJSON = require('csvtojson');
 const fs = require("fs");
 
-
 const yieldCSVPath: string = 'parsingFiles/yields.csv';
+const interventionCSVPath: string = 'parsingFiles/intervention.csv';
 
 interface JSONType {
     [key: string]: any
 }
 
-// convert users.csv file to JSON array
+let interventionMap: JSONType = {};
+
+console.log('Creating interventionMap');
+CSVToJSON().fromFile(interventionCSVPath)
+    .then((interventions: any) => {
+        for (let intervention of interventions) {
+            // removing all whitespace from keys (this is done as there is some parsing error that occurs when keys contain whitespace)
+            let interventionTitle = intervention.sKey.replace(/\s+/g, "");
+            interventionMap[interventionTitle] = intervention.key
+        }
+        console.log(interventionMap);
+        console.log('Cultivar mix: ' + interventionMap['Cultivarmix']);
+    })
+    .catch((e: any) => {
+        console.log("intervention map creation error" + e);
+    })
+// Script that converts yields.csv to yields.json with proper headers
 CSVToJSON().fromFile(yieldCSVPath)
     .then((yields: any) => {
         let jsonArray = [];
         for (let yield of yields) {
             let jsonObj: JSONType = {};
-            // console.log(yield)
-            // jsonObj['coords'] = {}
             let coordsObj: JSONType = {};
             coordsObj['type'] = 'Point'
             coordsObj['coordinates'] = [yield.Study_Longitude, yield.Study_Latitude]
@@ -23,7 +37,10 @@ CSVToJSON().fromFile(yieldCSVPath)
             jsonObj['effectSize'] = yield.EffectSize_ReportedValue;
             jsonObj['sampleSize'] = yield['Ncontrol']['new'];
             jsonObj['importID'] = '1';
-            jsonObj['interventionType'] = yield['Intervention_type']['new']; // needs revisino
+            jsonObj['interventionType'] = JSON.parse(JSON.stringify(interventionMap))[String(yield['Intervention_type']['new']).replace(/\s+/, "")]; // needs revision
+            // console.log(interventionMap)
+            console.log(yield['Intervention_type']['new'])
+            console.log(jsonObj['interventionType'])
 
             let filterObj: JSONType = {};
             filterObj['author'] = yield.Study_Authors;
@@ -34,21 +51,18 @@ CSVToJSON().fromFile(yieldCSVPath)
 
             jsonObj['filterCols'] = filterObj;
 
-            // console.log(jsonObj);
             jsonArray.push(jsonObj);
         }
+        // console.log(jsonArray)
 
-        fs.writeFile('./yields.json', JSON.stringify(jsonArray), ((err: any) => {
+        fs.writeFile('parsingFiles/yields.json', JSON.stringify(jsonArray), ((err: any) => {
             if(err) {
                 console.log(err);
           } 
           else {
             console.log("Output saved to parsingFiles/yields.json.");
             }
-        }))
-    // });
-        
+        }))        
     }).catch((err: any) => {
-        // log error if any
         console.log(err);
     });
